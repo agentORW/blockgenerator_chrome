0c9664bed03e84ed911475a2038e607bd296b5f1
agentORW <oskar.wisnes@gmail.com>
2025-02-19 13:20:04 +0100

    Updated readme

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # blockgenerator_chrome
 A chrome extension where you select two pixels from your screen and it generates the three closest blocks (from minecraft version 1.12.2) Hope you like it!
+
+## How to use
+
+First, you pick the brightest and darkest color of the building:
+
+![Selecting color](./assets/Pick_color.png)
+
+Then, click generate!
+
+![Generating color](./assets/Generate%20color.png)
--- contentScript.js
+++ contentScript.js
@@ -1,6 +1,8 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 
-    if (message.from === "popup" && message.query === "bright_eye_dropper_clicked") {
+    let storage = message.query === "bright_eye_dropper_clicked" ? "bright_color_hex_code" : "dark_color_hex_code";
+
+    if (message.from === "popup" && (message.query === "bright_eye_dropper_clicked" || message.query === "dark_eye_dropper_clicked")) {
 
         setTimeout(() => {
 
@@ -10,27 +12,11 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 
                 console.log(result);
 
-                chrome.storage.local.set({ "bright_color_hex_code": [result.sRGBHex] })
+                set_storage = {};
+                set_storage[storage] = [result.sRGBHex];
 
+                chrome.storage.local.set(set_storage)
 
-            }).catch(e => {
-                console.log(e)
-            })
-
-        }, 500);
-    }
-
-    if (message.from === "popup" && message.query === "dark_eye_dropper_clicked") {
-
-        setTimeout(() => {
-
-            const eyeDropper = new EyeDropper();
-
-            eyeDropper.open().then(result => {
-
-                console.log(result);
-
-                chrome.storage.local.set({ "dark_color_hex_code": [result.sRGBHex] })
 
             }).catch(e => {
                 console.log(e)
